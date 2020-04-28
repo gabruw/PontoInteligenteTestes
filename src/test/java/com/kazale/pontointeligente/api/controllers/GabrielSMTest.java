9a9da1d0226eae7e406ba7d3b21988c419f27b35
Gabriel Moreira <smoreira.gabriel@gmail.com>
2020-04-27 23:37:13 -0300

    Caso de teste para verificar o método que buscarPorCnpj da classe EmpresaController

---
+++ src/test/java/com/kazale/pontointeligente/api/controllers/GabrielSMTest.java
@@ -0,0 +1,53 @@
+package com.kazale.pontointeligente.api.controllers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.kazale.pontointeligente.api.dtos.EmpresaDto;
+import com.kazale.pontointeligente.api.entities.Empresa;
+import com.kazale.pontointeligente.api.response.Response;
+import com.kazale.pontointeligente.api.services.EmpresaService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class GabrielSMTest {
+
+	@Mock
+	EmpresaService service;
+
+	@InjectMocks
+	EmpresaController controller;
+
+	Empresa empresa;
+
+	@Before
+	public void setup() {
+		empresa = new Empresa();
+		empresa.setId(1l);
+		empresa.setCnpj("123456");
+		empresa.setRazaoSocial("Teste razão social");
+		when(service.buscarPorCnpj(anyString())).thenReturn(Optional.of(empresa));
+	}
+
+	@Test
+	public void buscarPorCnpjTest() {
+		assertThat(controller.buscarPorCnpj("123456").getBody()).isNotNull();
+		assertEquals(HttpStatus.OK, controller.buscarPorCnpj("123456").getStatusCode());
+	}
+}
